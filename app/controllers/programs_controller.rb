a77a0dceb60644b5f2fe9a334198401e75635e13
jules fagnoni <fagnonijules@gmail.com>
2020-08-28 12:44:12 +0200

    program generation

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,7 +1,16 @@
 class ProgramsController < ApplicationController
+
   def create
+    @user = current_user
+    @init_smoke = @user.average_smoke
+    @start_time = Date.current
+    Program.create(user: @user, init_smoke: @init_smoke, start_time: @start_time)
+    redirect_to my_program_path
   end
 
-  def show
+  private
+
+  def program_params
+    params.require(:program).permit(:user, :init_smoke, :start_time)
   end
 end
