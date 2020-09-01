d81af2245bdb22dcd230cfc342cfc8f76adc38d7
jules fagnoni <fagnonijules@gmail.com>
2020-09-01 14:42:26 +0200

    add cigarette button ok

---
+++ app/javascript/components/add_clope.js
@@ -0,0 +1,10 @@
+const addClope = () => {
+  const btn = document.querySelector("#add-btn")
+  const smoked = document.querySelector(".smoked-a")
+  if (btn) {
+    btn.addEventListener("click", (event) => {
+    })
+  }
+}
+
+export { addClope };
