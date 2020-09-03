bd5f19849a239b36760d9f037c29cb6c9ab0defb
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-09-03 18:42:39 +0200

    bug fix

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -3,6 +3,7 @@ require 'httparty'
 
 class CaseController < ActionController::Base
   def unlock
+    Smoke.create(user:current_user)
     HTTParty.post("#{Case.first.url}/unlock")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -274,10 +274,12 @@ end
 6.times do |i|
   Smoke.create(user: romain, created_at: program_date_launch_romain + 16.day)
 end
-2.times do |i|
+5.times do |i|
   Smoke.create(user: romain, created_at: program_date_launch_romain + 17.day)
 end
-
+2.times do |i|
+  Smoke.create(user: romain, created_at: program_date_launch_romain + 18.day)
+end
 
 
 require 'program_seed_creator'
