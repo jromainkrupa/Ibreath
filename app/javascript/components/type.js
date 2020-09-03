128311adaafccddba3bc005bdf4b57934231eeaf
Matthieu Carlhian <matthieu.carlhian@gmail.com>
2020-09-03 13:46:00 +0200

    oui oui

--- app/javascript/components/type.js
+++ app/javascript/components/type.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["1 month", "30 days", "720 hours"],
+    strings: ["1 month", "30 days", "720 hours."],
     typeSpeed: 80,
     loop: true
   });
